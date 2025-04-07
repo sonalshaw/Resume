020f1336f70b5f9f4ca7d38762d096a16268ebf9
sonalshaw <72877298+sonalshaw@users.noreply.github.com>
2025-04-08 00:11:16 +0530

    Create data.js

---
+++ JS/data.js
@@ -0,0 +1,45 @@
+const profileData = {
+    name: "Sonal Shaw",
+    about: "I'm a data science engineer with nearly 3 years of experience, passionate about creating impactful solutions. I lead collaboratively, uniting teams, and blend strategic thinking with hands-on work. Eager to learn and adapt, I aim to drive results and grow in a meaningful career.",
+    contact: {
+        address: "Rourkela, Odisha - 769004",
+        phone: "+91-6370039436",
+        email: "sonalshaw237@gmail.com",
+        linkedin: "https://www.linkedin.com/in/sonal-shaw-691802207/"
+    },
+    experience: [
+        {
+            company: "Carelon Healthcare Services",
+            role: "Associate Software Engineer I",
+            duration: "July 2022 – Present",
+            responsibilities: [
+                "Developed and maintained ETL processes using Python for efficient data extraction, transformation, and loading.",
+                "Created scripts for retrieving and transforming large datasets from Hadoop clusters, ensuring seamless data flow.",
+                "Handled data retrieval from Hadoop, optimizing query performance and data processing workflows.",
+                "Managed Control-M job scheduling to automate and monitor data processing tasks, ensuring minimal downtime.",
+                "Monitored and maintained logs for data processing workflows, resolving issues promptly to maintain system stability.",
+                "Led AWS code deployments using Bitbucket, managing continuous integration and delivery pipelines.",
+                "Wrote complex SQL queries for data validation, reconciliation, and reporting, ensuring high data accuracy.",
+                "Administered and maintained the AEDL portal, ensuring secure and efficient access to enterprise data resources.",
+                "Designed and optimized Snowflake data loading processes through AWS, improving data storage and retrieval efficiency."
+            ]
+        }
+    ],
+    strengths: [
+        "Proven ability to meet tight deadlines and deliver high-quality results under pressure.",
+        "Excellent communication and presentation skills, capable of conveying complex technical concepts to diverse audiences.",
+        "Strong team player with demonstrated leadership abilities, fostering collaboration and driving team success."
+    ],
+    skills: [
+        "JAVA", "Python", "SQL",
+        "HTML", "CSS", "JavaScript",
+        "Data Structures", "Web Development", "Hadoop",
+        "Snowflake", "AWS", "Control-M"
+    ],
+    education: {
+        degree: "B.Tech (Computer Science Engineering)",
+        institution: "ITER - BBSR, Odisha",
+        year: "2018 – 2022",
+        cgpa: "8.37"
+    }
+};
